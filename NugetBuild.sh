825c598027e4c183e18b12986f6b7a570e752dcd
Pedro Rodriguez <rod.rizo.pedro@gmail.com>
2021-12-01 23:17:21 -0500

    Changed Nuget format

--- NugetBuild.sh
+++ NugetBuild.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
+SUFFIX=`date +"%y-%j-%H%M%S"`
 
-RandomString () {
-	cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-7} | head -n 1
-}
-
-SUFFIX=`RandomString`
-
-dotnet pack -c Release -o nupkgs --version-suffix ${SUFFIX}
-
-
+dotnet pack -c Release -o nupkgs -p:VersionSuffix=${SUFFIX}
